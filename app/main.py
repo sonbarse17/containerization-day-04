a58796f2caaebadf1c2dfb043f8470e837a3524b
sonbarse17 <sushantsonbarse90@gmail.com>
2026-02-15 15:34:44 +0530

    fix lint issue

--- app/main.py
+++ app/main.py
@@ -8,7 +8,8 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "App is running"}
- 
+
+
 @app.get("/add/{a}/{b}")
 def add_numbers(a: int, b: int):
     return {"result": add(a, b)}
